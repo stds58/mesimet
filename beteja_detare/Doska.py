70e1a00bdaf49a1fb6ae6d59e17205fff151eaa6
stds58 <145394899+stds58@users.noreply.github.com>
2023-10-23 00:22:55 +0300

    Update Doska.py

--- beteja_detare/Doska.py
+++ beteja_detare/Doska.py
@@ -159,7 +159,6 @@ class Doska():
             if x[3] in ['В', 'П', 'Н', 'Л']:
                 b = Ship(x[0],x[1],x[2],x[3])
                 self.ustanovit_korabl(b.korabl)
-                self.obvodka_korabla(b.korabl)
                 if self.status:
                     print(self.status)
                 if self.status:
