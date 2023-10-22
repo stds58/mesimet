a0006736bb54c8969ec8df2ceec93e90c83a60fd
stds58 <145394899+stds58@users.noreply.github.com>
2023-10-22 20:18:38 +0300

    Update Doska.py

--- beteja_detare/Doska.py
+++ beteja_detare/Doska.py
@@ -1,78 +1,232 @@
 
-class Doska:
-    storona_min = 4
-    storona_max = 20
-    def __init__(self, storona = None, oshibka_proverka = False):
+from random import randint
+from Ship import Ship
+
+class Doska():
+    def __init__(self, storona, auto = False):
         self.storona = storona
         self.doska = self.set_doska
-        self.oshibka = self.get_oshibka
-        self.cell = self.get_proverka_cell
-        self.oshibka_proverka = oshibka_proverka
+        self.status = self.ustanovit_korabl
+        self.kolvo_ship = [1, 4, 2, 1]
+        self.spisok_ship = []
+        self.auto = auto
 
-    #обработка ошибок
-    @property
-    def get_oshibka(self):
-        if not isinstance(self.storona, int):
-            self.oshibka = f'ошибка: сторона должна быть целым числом от {self.storona_min} до {self.storona_max}'
-        else:
-            if self.storona < self.storona_min or self.storona > self.storona_max:
-                self.oshibka = f'ошибка: сторона должна быть от {self.storona_min} до {self.storona_max}'
-            else:
-                self.oshibka = False
-        return self.oshibka
 
     #сделать доску
     @property
     def set_doska(self):
-        if not self.get_oshibka:
-            d = range(1, self.storona + 1)
-            self.doska = [[' О ' for i in d] for j in d]
-        else:
-            self.doska = []
+        d = range(1, self.storona + 1)
+        self.doska = [[' О ' for i in d] for j in d]
         return self.doska
+
+
+    def set_doska_dublazh(self, obj_doska):
+        self.doska_dublazh = []  # дубляж доски из которой убираем занятые клетки и клетки которые уже перебрали для расстановки
+        for i in obj_doska:
+            L_temp = []
+            for j in i:
+                L_temp.append(j)
+            self.doska_dublazh.append(L_temp)
+        self.len_doska = len(obj_doska)
+        L_shablon = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 0], [0, 1], [1, -1], [1, 0], [1, 1]]
+        # удалить занятые ячейки
+        for i1, i2 in zip(self.doska_dublazh, range(self.len_doska)):
+            for j1, j2 in zip(i1, range(self.len_doska)):
+                if j1 == ' ■ ':
+                    self.doska_dublazh[i2].pop(j2)
+        return self.doska_dublazh
 
     #показать доску в удобном виде
     def __str__(self):
-        if not self.get_oshibka:
-            stroka = ' |'+'|'.join([f' {i} ' for i in range(1,self.storona+1)])+'|\n'
-            stroka = stroka + '\n'.join([f"{ii}|{'|'.join(i)}|" for i,ii in zip(self.doska,range(1,self.storona+1))])
-            #return '\n'.join([f"{ii}|{'|'.join(i)}|" for i,ii in zip(self.doska,range(1,self.storona+1))])
-            return stroka
+        doska_tuman = []
+        for i1, i2 in zip(self.doska, range(self.storona)):
+            L2 = []
+            for j1, j2 in zip(i1, range(self.storona)):
+                if j1 == ' ■ ':
+                    L2.append(' О ')
+                else:
+                    L2.append(j1)
+            doska_tuman.append(L2)
+        if self.auto:
+            stroka = '  |' + '|'.join([f'{i:02} ' for i in range(1, self.storona + 1)]) + '|\n'
+            self.stroka = stroka + '\n'.join(
+                [f"{ii:02}|{'|'.join(i)}|" for i, ii in zip(doska_tuman, range(1, self.storona + 1))])
         else:
-            return self.get_oshibka
+            stroka = '  |' + '|'.join([f'{i:02} ' for i in range(1, self.storona + 1)]) + '|\n'
+            self.stroka = stroka + '\n'.join(
+                [f"{ii:02}|{'|'.join(i)}|" for i, ii in zip(self.doska, range(1, self.storona + 1))])
+        return self.stroka
 
-    #проверить входит ли значение в диапазон доски и получить значение клетки
-    def set_proverka_cell(self, x, y):
-        try:
-            self.cell = self.doska[x][y]
-        except IndexError as e:
-            self.oshibka_proverka = True  #'либо доска пустая либо координаты за гранью доски'
-        else:
-            self.oshibka_proverka = False
+    #проверка корабля
+    def ustanovit_korabl(self, obj_ship):
+        #obj_ship = b.korabl [[4, 2], [3, 2], [2, 2]]
+        self.status = False
+        # проверить корабль
+        while not self.status:
+            for i in obj_ship:
+                if i[0] < 0 or i[0] >= self.storona or i[1] < 0 or i[1] >= self.storona:
+                    self.status = "клетка за гранью доски"
+            break
 
-    def get_proverka_cell(self):
-        return self.cell
+        # установить область вокруг корабля
+        self.oblast = []
+        L_shablon = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 0], [0, 1], [1, -1], [1, 0], [1, 1]]
+        for i in obj_ship:
+            for j in L_shablon:
+                a = i[0] + j[0]
+                b = i[1] + j[1]
+                if [a, b] not in self.oblast and a >= 0 and b >= 0 and a < self.storona and b < self.storona:
+                    self.oblast.append([a, b])
 
-    def get_proverka_oshibka(self):
-        return self.oshibka_proverka
+        # проверить область корабля
+        for i in self.oblast:
+            if self.doska[i[0]][i[1]] == ' ■ ':
+                self.status = 'клетка занята'
+                break
+
+        # проверить количество кораблей
+        if not self.status:
+            dlina = len(obj_ship)
+            if not self.kolvo_ship[dlina]:
+                self.status = 'корабль такой длины  больше уже нельзя поставить'
+
+        # запись допустимого корабля
+        if not self.status:
+            for i in obj_ship:
+                self.doska[i[0]][i[1]] = ' ■ '
+            self.kolvo_ship[dlina] -= 1
+        # подсчёт кораблей
+        if not self.status:
+            sum = 0
+            for i in range(1, len(self.kolvo_ship)):
+                sum = sum + self.kolvo_ship[i]
+                # print(sum)
+            if not sum:
+                self.kolvo_ship[0] = 0
+                self.status = 'все корабли расставлены'
+        return self.status
 
 
+    def proverka_int(self, tekst, x_min, x_max):
+        x = input(tekst)
+        while True:
+            try:
+                a = int(x)
+                if a >= x_min and a <= x_max:
+                    return int(x)
+                else:
+                    x = input(f'значение должно быть от {x_min} до {x_max} \n{tekst}')
+            except ValueError as e:
+                x = input(tekst)
+            if not ValueError:
+                break
 
-#IndexError
+    def proverka_abc(self, tekst):
+        x = input(tekst)
+        while x not in ['В', 'П', 'Н', 'Л', 'З']:
+            x = input(f'значение должно быть В верх, П право, Н низ, Л лево, З начать всё заново:')
+        return x
 
-# pl1 = Doska(6)
-# print(pl1.doska[1][3])
-# pl1.set_proverka_cell([[3,4],[1,4],[2,4]])
-# print( pl1.get_proverka_cell() )
-# #x = input('ввести значение:')
-# x = 5
-# y = 2
-# if x-1 >= 0 and x-1 < pl1.storona and y-1 >= 0 and y-1 < pl1.storona:
-#     pl1.doska[x-1][y-1] = ' ■ '
-# print(pl1)
-# print('--------------')
-#print(pl1.storona)
+    def vvod_klaviatura(self):
+        # ввести координаты
+        dlina = self.proverka_int('длина корабля:', 1, 3)
+        x = self.proverka_int('x по вертикали:', 1, self.storona)
+        y = self.proverka_int('y по горизонтали:', 1, self.storona)
+        napravlenie = self.proverka_abc('введите В верх, П право, Н низ, Л лево, З начать всё заново:')
+        return dlina, x, y, napravlenie
+
+    def igrok_rasstanovka(self):
+        while True:
+            x = self.vvod_klaviatura()
+            if x[3] in ['В', 'П', 'Н', 'Л']:
+                b = Ship(x[0],x[1],x[2],x[3])
+                self.ustanovit_korabl(b.korabl)
+                if self.status:
+                    print(self.status)
+                if self.status:
+                    print('self.kolvo_ship', self.kolvo_ship)
+                    print(self.__str__())
+                if self.status == 'все корабли расставлены':
+                    break
 
 
+    def auto_rasstanovka(self):
+        self.status = 'начало'
+        doska_dubl = self.set_doska_dublazh(self.doska)
+        i = 1
+        while self.status:
+            b = Ship(0, 0, 0, '')
+            self.x = b.rand_ship(self.doska, doska_dubl, self.kolvo_ship)
+            b = Ship(self.x[0], self.x[1], self.x[2], self.x[3])
+            self.ustanovit_korabl(b.korabl)
+            if not self.status:
+                self.spisok_ship.append(b)
+            if self.status == 'все корабли расставлены':
+                self.spisok_ship.append(b)
+                break
+            i += 1
+            if i > self.storona**2 and self.kolvo_ship[0] == 1:
+                self.spisok_ship = []
+                self.kolvo_ship = [1, 4, 2, 1]
+                self.doska = self.set_doska
+                doska_dubl = self.set_doska_dublazh(self.doska)
+                self.status = 'начало'
+                i = 1
+            elif i <= self.storona**2 and self.kolvo_ship[0] == 1:
+                self.status = 'начало'
+
+    # ход одного игрока
+    def hod(self, auto = False):
+        while len(self.spisok_ship):
+            if auto:
+                x, y = self.autohod()
+            else:
+                print('буквой X помечаются подбитые корабли, буквой T — промахи')
+                x = self.proverka_int('введите координаты клетки.х по вертикали:', 1, self.storona) - 1
+                y = self.proverka_int('введите координаты клетки.у по горизонтали:', 1, self.storona) - 1
+            if self.doska[x][y] == ' ■ ':
+                self.doska[x][y] = ' X '
+                l = self.spisok_ship
+                for i, j in zip(l, range(len(l))):  # взять корабль из списка [[2, 2], [2, 3], [2, 4]]
+                    l_ship = self.spisok_ship[j].korabl
+                    for ii, jj in zip(l_ship, range(len(l_ship))):  # взять координаты корабля [2, 2]
+                        if [x, y] == ii:
+                            self.spisok_ship[j].korabl.pop(jj)
+                            #print(self.spisok_ship[j].korabl)
+                            if not self.spisok_ship[j].korabl:
+                                self.spisok_ship.pop(j)
+                                print(self.__str__())
+                                print('подбит')
+                                if not len(self.spisok_ship):
+                                    print('победа')
+                            else:
+                                print(self.__str__())
+                                print('попадание')
+            else:
+                self.doska[x][y] = ' T '
+                print('промах\n')
+                break
+            #print(obj)
+            #print('-------------')
+            # print(pl2.spisok_ship[0].korabl)
+            # print(pl2.spisok_ship[1].korabl)
+            # print(pl2.spisok_ship[2].korabl)
+            # print(pl2.spisok_ship[3].korabl)
+            # print(pl2.spisok_ship[4].korabl)
+            # print(pl2.spisok_ship[5].korabl)
+            # print(pl2.spisok_ship[6].korabl)
+
+    # ход компа
+    def autohod(self):
+        L = []
+        # создать список доступных координат
+        for i1, i2 in zip(self.doska, range(self.storona)):
+            for j1, j2 in zip(i1, range(self.storona)):
+                if j1 == ' О ' or j1 == ' ■ ':
+                    L.append([i2, j2])
+        i = randint(0, len(L)-1)
+        x = L[i][0]
+        y = L[i][1]
+        return x, y
 
 
